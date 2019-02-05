d6afdf62a792f7cef285f7b892c01ec097ca900d
Anas Louter <anas.louter@ensimag.grenoble-inp.fr>
2019-02-05 15:32:37 +0100

    none

--- runAndroid.sh
+++ runAndroid.sh
@@ -1,3 +1,3 @@
-export ANDROID_HOME=/home/desobryd/Android/Sdk
+export ANDROID_HOME=/home/ananas/Downloads/ProjetGCRequires/SDK
 export PATH=$PATH:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools
 react-native run-android
