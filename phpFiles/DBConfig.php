2d27cbf2e21685376022a1a17e1e7049edbeaedb
Anas Louter <anas.louter@ensimag.grenoble-inp.fr>
2019-02-16 15:33:10 +0100

    added phpFiles, in order to change log in config

---
+++ phpFiles/DBConfig.php
@@ -0,0 +1,15 @@
+<?php
+
+//Define your host here.
+$HostName = "servername";
+
+//Define your database name here.
+$DatabaseName = "dbname";
+
+//Define your database username here.
+$HostUser = "username";
+
+//Define your database password here.
+$HostPass = "password";
+
+?>
--- runAndroid.sh
+++ runAndroid.sh
@@ -1,3 +1,6 @@
-export ANDROID_HOME=/home/ananas/Downloads/ProjetGCRequires/SDK
-export PATH=$PATH:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools
+export ANDROID_HOME=$HOME/home/ananas/Downloads/ProjetGCRequires/SDK
+export PATH=$PATH:$ANDROID_HOME/emulator
+export PATH=$PATH:$ANDROID_HOME/tools
+export PATH=$PATH:$ANDROID_HOME/tools/bin
+export PATH=$PATH:$ANDROID_HOME/platform-tools
 react-native run-android
